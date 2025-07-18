86240ec3dc6a1e1940e77d4833c5df335795d1b7
unknown <chaurasiyaomshivam9918@gmail.com>
2025-07-18 12:18:47 +0530

    Update product detail page and switch to online product data

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// app.use(cors());
+// app.use(express.json());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 
 // ✅ MongoDB Atlas connection without deprecated options
 mongoose.connect(process.env.MONGO_URI)
